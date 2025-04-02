e73d47ee2306ce7bb417984e2bf3fa348b96bcab
beclarkkv <beclark@kinetic-vision.com>
2025-04-02 10:54:25 -0400

    refactor(fetch_data): update project JSON processing

--- src/jira_copy/fetch_data.py
+++ src/jira_copy/fetch_data.py
@@ -11,8 +11,30 @@ def get_project_json(key: str) -> dict:
 
     return response.json()
 
-def get_project_id(key: str) -> int:
-    return get_project_json(key).get("id")
+
+def get_source_json():
+    response = get_project_json(source_project_key)
+    with open("source-project.json", "w") as source_file:
+        json.dump(response, source_file, indent=4)
+
+
+def get_target_json():
+    response = get_project_json(target_project_key)
+    with open("target-project.json", "w") as target_file:
+        json.dump(response, target_file, indent=4)
+
+
+def get_source_id():
+    with open("source-project.json") as source_file:
+        text = json.load(source_file)
+        return text.get("id", -1)
+
+
+def get_target_id():
+    with open("target-project.json") as target_file:
+        text = json.load(target_file)
+        return text.get("id", -1)
+
 
 def get_project_issue_types(project_id: int) -> dict:
     url = base_url + "/rest/api/3/issuetype/project"
@@ -29,17 +51,20 @@ def get_project_issue_types(project_id: int) -> dict:
 
     return response.json()
 
+
 def get_source_issue_types():
-    project_id = get_project_id(source_project_key)
+    project_id = get_source_id()
     response = get_project_issue_types(project_id)
     with open('source-issue-types.json', 'w') as source_file:
         json.dump(response, source_file, indent=4)
+
     
 def get_target_issue_types():
-    project_id = get_project_id(target_project_key)
+    project_id = get_target_id()
     response = get_project_issue_types(project_id)
     with open('target-issue-types.json', 'w') as target_file:
         json.dump(response, target_file, indent=4)    
+
 
 def get_project_statuses(project_id: int ) -> dict:
     url = base_url + "/rest/api/3/statuses/search"
@@ -54,14 +79,16 @@ def get_project_statuses(project_id: int ) -> dict:
 
     return response.json()
 
+
 def get_source_statuses():
-    project_id = get_project_id(source_project_key)
+    project_id = get_source_id()
     response = get_project_statuses(project_id)
     with open("source-statuses.json", "w") as source_file:
         json.dump(response, source_file, indent=4)
 
+
 def get_target_statuses():
-    project_id = get_project_id(target_project_key)
+    project_id = get_target_id()
     response = get_project_statuses(project_id)
     with open("target-statuses.json", "w") as target_file:
         json.dump(response, target_file, indent=4)
