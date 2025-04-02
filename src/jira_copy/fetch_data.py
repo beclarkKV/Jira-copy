fa7d6e6f38e82115698cf66468aa910371088bf4
beclarkkv <beclark@kinetic-vision.com>
2025-04-02 10:25:18 -0400

    feat(fetch_data): retrieve statuses for source and target projects

--- src/jira_copy/fetch_data.py
+++ src/jira_copy/fetch_data.py
@@ -41,5 +41,28 @@ def get_target_issue_types():
     with open('target-issue-types.json', 'w') as target_file:
         json.dump(response, target_file, indent=4)    
 
+def get_project_statuses(project_id: int ) -> dict:
+    url = base_url + "/rest/api/3/statuses/search"
 
+    headers = {
+      "Accept": "application/json"
+    }
+
+    params = {"projectId": project_id}
+
+    response = GET(url, headers, params)
+
+    return response.json()
+
+def get_source_statuses():
+    project_id = get_project_id(source_project_key)
+    response = get_project_statuses(project_id)
+    with open("source-statuses.json", "w") as source_file:
+        json.dump(response, source_file, indent=4)
+
+def get_target_statuses():
+    project_id = get_project_id(target_project_key)
+    response = get_project_statuses(project_id)
+    with open("target-statuses.json", "w") as target_file:
+        json.dump(response, target_file, indent=4)
 
